db11ed8c7b15119ae7bf402008d2770d6308e2c0
Annan Dejene <annandejene2020@gmail.com>
2022-07-19 16:27:05 -0700

    Completed the function that takes city name and gives the coordinates

--- Weather-Fetcher/weather.py
+++ Weather-Fetcher/weather.py
@@ -1,1 +1,28 @@
+from platform import java_ver
+from urllib import response
+import requests
+import json
+
+
+# API Key provided by openweathermap.org to fetch wether data
 API_KEY = 'd01b273d7cc1a73855e86c432fc39b97'
+
+
+def coordinate_from_city(city_name, API_KEY):
+    params = {'q': city_name, 'appid': API_KEY}
+    url = 'http://api.openweathermap.org/geo/1.0/direct'
+    r = requests.get(url=url, params=params)
+    json_data = r.json()[0]
+
+    return json_data['lat'], json_data['lon']
+
+
+city = input('Enter your city: ')
+(lat, lon) = coordinate_from_city(city_name=city, API_KEY=API_KEY)
+
+params = {'lat': lat, 'lon': lon, 'appid': API_KEY}
+url = 'https://api.openweathermap.org/data/2.5/weather'
+
+respons = requests.get(url=url, params=params)
+
+print(respons.json())
